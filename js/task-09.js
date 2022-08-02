9c1c2aa252a0f0a688bb9057e9e462a5f16db1a0
he1gi <oleh.mishchenko1@gmail.com>
2022-08-02 19:50:16 +0300

    completed task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btnEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function getRandomBackgroundColor() {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  textEl.textContent = randomColor;
+}
+
+btnEl.addEventListener("click", getRandomBackgroundColor);
